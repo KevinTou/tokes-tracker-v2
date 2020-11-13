b227eb1efc8268f7c416ba94586b65b001b631da
Kevin Tou <toukevin@gmail.com>
2020-11-12 18:19:14 -0800

    Added spacer and updated to use makeStyles

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core';
 
 // Components
 import { PrivateRoute } from '../utils/PrivateRoute';
@@ -9,9 +10,19 @@ import Login from './Login';
 import Navigation from './Navigation';
 import Register from './Register';
 
+const useStyles = makeStyles({
+  background: {
+    backgroundColor: '#3d3d3d',
+    height: '100vh',
+    width: '100vw',
+  },
+});
+
 function App() {
+  const classes = useStyles();
+
   return (
-    <>
+    <div className={classes.background}>
       <Navigation />
       <Switch>
         <Route exact path="/" component={Landing} />
@@ -20,7 +31,7 @@ function App() {
         <PrivateRoute path="/dashboard" component={Dashboard} />
         <Redirect to="/dashboard" />
       </Switch>
-    </>
+    </div>
   );
 }
 
