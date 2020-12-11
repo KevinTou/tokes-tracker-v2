fd41eb9fa7003a6c21f0e2ad708927cbe8698162
Kevin Tou <toukevin@gmail.com>
2020-12-10 16:02:27 -0800

    Adding landing page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,10 +1,38 @@
 import React from 'react';
 
+import { makeStyles } from '@material-ui/core';
+
+import { ReactComponent as CalculatorSVG } from '../images/calculator.svg'
+
+const useStyles = makeStyles(theme => ({
+  backgroundBanner: {
+    height: '40vh',
+    backgroundColor: 'white',
+    display: 'grid',
+  },
+  bannerImageContainer: {
+    width: '500px',
+    height: 'auto'
+  },
+  bannerImage: {
+    width: '100%',
+    height: '100%'
+  }
+}))
+
 const Landing = () => {
+  const classes = useStyles();
   return (
-    <div>
-      <h1>Landing</h1>
-    </div>
+    <>
+      <div className={classes.backgroundBanner}>
+        <div className={classes.bannerImageContainer}>
+          <CalculatorSVG className={classes.bannerImage} />
+        </div>
+        {/* <div>
+          <h2>Lorem</h2>
+        </div> */}
+      </div>
+    </>
   );
 };
 
