b95d2b017623df5f054c383dc24b924b89c6f0e4
Kevin Tou <toukevin@gmail.com>
2021-02-16 15:38:10 -0800

    Updated tokes details

--- src/components/TokesDetail.js
+++ src/components/TokesDetail.js
@@ -1,11 +1,57 @@
-import React from 'react'
+import React from 'react';
+import { connect } from 'react-redux';
+import {
+  makeStyles,
+  List,
+  Divider,
+  Container
+} from '@material-ui/core';
 
-const TokesDetail = () => {
+import Toke from './Toke';
+
+const useStyles = makeStyles((theme) => ({
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+  list: {
+    color: 'white',
+    height: '71vh',
+    overflow: 'auto',
+    margin: '1rem 0 0 0',
+  },
+  divider: {
+    backgroundColor: 'white',
+  },
+}));
+
+const TokesDetail = ({ tokes }) => {
+  const classes = useStyles();
+
+  const renderTokes = (tokes) => {
+    return tokes.map((toke, index) => (
+      <React.Fragment key={toke.id}>
+        {index !== 0 && <Divider className={classes.divider} />}
+        <Toke toke={toke} />
+      </React.Fragment>
+    ));
+  };
+
   return (
-    <div>
-      <h1>TokesDetails</h1>
-    </div>
-  )
+    <Container className={classes.content} component="main" maxWidth="xs">
+      <List className={classes.list} id="listed">
+        {renderTokes(tokes)}
+      </List>
+    </Container>
+  );
 }
 
-export default TokesDetail
+const mapStateToProps = state => {
+  const { tokes } = state.tokes;
+
+  return {
+    tokes: tokes
+  }
+}
+
+export default connect(mapStateToProps, {})(TokesDetail)
